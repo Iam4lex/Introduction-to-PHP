457021535128ddb56184b6eab80ff178fc554b9c
Alex Mwangi <mungaialexm@gmail.com>
2024-03-17 14:08:17 +0300

    first commit

---
+++ DataTypes.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // DataTypes of items are obtained using the var_dump() function
+        var_dump(10); // returns int(10)
+
+        print("<br><br>"); // print new line
+
+        var_dump("Hello world"); // returns String()
+
+        print("<br><br>"); // print new line
+
+        // boolean
+        $first_number = true;
+        var_dump($first_number);
+    ?>
+</body>
+</html>
--- php_variables.php
+++ php_variables.php
@@ -9,13 +9,41 @@
     <?php
         // php varibles begins with a dolar sign
 
-        $name = "Alex Mwangi";
+        // Integers
         $num1 = 10;
         $num2 = 10;
 
-        echo "My name is " + ( $num1 + $num2);
+        // Strings
+        $name = "Alex Mwangi";
 
+        $age = $num1 + $num2;
+
+        echo "My name is " .  $name . ". <br>I'm " . $age . " years old <br>";
+
+        //  DataTypes
+        $x = 5;
+        var_dump($x);
+
+        // Global variables
+        $x = 15; 
+
+        function test() {
+          // using x inside this function will generate an error
+        //   echo "<p>Variable x inside function is: $x </p>";
+        }
+        test();
         
+        echo "<p>Variable x outside function is: $x</p>";
+
+        // local varibles
+
+        function test2() {
+            $y = 10;
+            echo "<p>Variable inside a function is : $y</p>";
+        }
+        test2();
+        // This will generate an error
+        // echo "<p>Variable outside a function is : $y</p>";
     ?>
 </body>
 </html>
